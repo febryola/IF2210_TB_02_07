988878524117cfc0ebe871ae2c68ce0d27583f8d
chryes220 <christinehutabarat12@gmail.com>
2022-03-19 15:22:03 +0700

    implement tool

---
+++ src/tool.cpp
@@ -0,0 +1,20 @@
+// tool.cpp
+
+#include "tool.hpp"
+
+tool::tool() : item() {
+    durability = 0;
+}
+
+tool::tool(int id, string name, string type, int quantity, int durability) : item(id, name, type, quantity) {
+    this->durability = durability;
+}
+
+int tool::getDurability() {
+    return durability;
+}
+
+void tool::useTool() {
+    durability--;
+    // kalau durability = 0, harus dihapus dari inventory
+}
