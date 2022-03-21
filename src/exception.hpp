f0ad20a7347c8cf7e71faa6ca4fba731c314fe56
ravaattar <rava.naufal2014@gmail.com>
2022-03-21 16:38:31 +0700

    update exception header

--- src/exception.hpp
+++ src/exception.hpp
@@ -2,23 +2,78 @@
 #define _EXCEPTION_HPP_
 
 #include <iostream>
+#include <string>
 using namespace std;
-
-class Exception {
+class BaseException {
     public:
         virtual void printMessage() = 0;
 };
 
-class FullInventoryException : public Exception {
-
+class CraftIndexOutOfBoundException : public BaseException {
+    private:
+        int idxAccessed;
+        int craftSize;
+    public:
+        CraftIndexOutOfBoundException(int idxAccessed, int craftSize);
+        void printMessage();
+};
+ 
+class InventoryIndexOutOfBoundException : public BaseException {
+    private:
+        int idxAccessed;
+        int inventorySize;
+    public:
+        InventoryIndexOutOfBoundException(int idxAccessed, int inventorySize);
+        void printMessage();
 };
 
-class NonStackableException : public Exception {
-    
+// DI-HOLD DULU
+class FullInventoryException : public BaseException {
+    private:
+        string msg;
+    public:
+        FullInventoryException();
+        void printMessage();
 };
 
-class FullStackException : public Exception {
+class NonStackableException : public BaseException {
+    private:
+        string msg;
+    public:
+        NonStackableException();
+        void printMessage();
+};
+class FullStackException : public BaseException {
+    private:
+        string msg;
+    public:
+        FullStackException();
+        void printMessage();
+};
 
+class InvalidDiscardException : public BaseException {
+    private:
+        int itemQty;
+        int discardQty;
+    public:
+        InvalidDiscardException (int itemQty, int discardQty);
+        void printMessage();
+};
+
+class EmptyDiscardException : public BaseException {
+    private:
+        int itemQty;
+    public:
+        EmptyDiscardException (int itemQty);
+        void printMessage();
+};
+
+class EmptyUseException : public BaseException {
+    private:
+        int itemQty;
+    public:
+        EmptyUseException (int itemQty);
+        void printMessage();
 };
 
 #endif
